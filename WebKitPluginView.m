d5eb393956917454f167f121da76a42bb689abb6
Eloy Duran <eloy.de.enige@gmail.com>
2011-01-09 23:53:40 +0100

    Implement plug-in protocol methods in Ruby.

--- WebKitPluginView.m
+++ WebKitPluginView.m
@@ -1,10 +1,6 @@
 #import "WebKitPluginView.h"
 #import <MacRuby/MacRuby.h>
 
-//@interface WebKitPluginView (Internal)
-//- (id)_initWithArguments:(NSDictionary *)arguments;
-//@end
-
 @implementation WebKitPluginView
 
 // WebPlugInViewFactory protocol
@@ -12,63 +8,11 @@
 
 + (NSView *)plugInViewWithArguments:(NSDictionary *)newArguments
 {
+  // TODO Loading the Ruby source file probably only needs to happen once.
   NSBundle *bundle = [NSBundle bundleForClass:[WebKitPluginView class]];
   NSURL *url = [bundle URLForResource:@"WebKitPluginView" withExtension:@"rb"];
   [[MacRuby sharedRuntime] evaluateFileAtURL:url];
   return [[self alloc] performSelector:@selector(initWithArguments:) withObject:newArguments];
 }
 
-// WebPlugIn informal protocol
-
-//- (void)webPlugInInitialize
-//{
-    //// This method will be only called once per instance of the plug-in object, and will be called
-    //// before any other methods in the WebPlugIn protocol.
-    //// You are not required to implement this method.  It may safely be removed.
-//}
-
-//- (void)webPlugInStart
-//{
-    //// The plug-in usually begins drawing, playing sounds and/or animation in this method.
-    //// You are not required to implement this method.  It may safely be removed.
-//}
-
-//- (void)webPlugInStop
-//{
-    //// The plug-in normally stop animations/sounds in this method.
-    //// You are not required to implement this method.  It may safely be removed.
-//}
-
-//- (void)webPlugInDestroy
-//{
-    //// Perform cleanup and prepare to be deallocated.
-    //// You are not required to implement this method.  It may safely be removed.
-//}
-
-//- (void)webPlugInSetIsSelected:(BOOL)isSelected
-//{
-    //// This is typically used to allow the plug-in to alter its appearance when selected.
-    //// You are not required to implement this method.  It may safely be removed.
-//}
-
-//- (id)objectForWebScript
-//{
-    //// Returns the object that exposes the plug-in's interface.  The class of this object can implement
-    //// methods from the WebScripting informal protocol.
-    //// You are not required to implement this method.  It may safely be removed.
-    //return self;
-//}
-
 @end
-
-//@implementation WebKitPluginView (Internal)
-
-//- (id)_initWithArguments:(NSDictionary *)newArguments
-//{
-    //if (!(self = [super initWithFrame:NSZeroRect]))
-        //return nil;
-    
-    //return self;
-//}
-
-//@end
--- WebKitPluginView.rb
+++ WebKitPluginView.rb
@@ -1,9 +1,47 @@
 # Superclass NSTextField is already defined in WebKitPluginView.h
 class WebKitPluginView
-  def initWithArguments(dictionary)
+  def initWithArguments(attributes)
     if initWithFrame(NSZeroRect)
-      p dictionary
+      puts "Initialized with attributes: #{attributes.description}"
       self
     end
   end
+
+  # WebPlugIn informal protocol
+  #
+  # All these methods are not required and can safely be removed.
+
+  # This method will be only called once per instance of the plug-in object, and will be called
+  # before any other methods in the WebPlugIn protocol.
+  def webPlugInInitialize
+    puts "[!] webPlugInInitialize called"
+  end
+
+  # The plug-in usually begins drawing, playing sounds and/or animation in this method.
+  def webPlugInStart
+    puts "[!] webPlugInStart called"
+  end
+
+  # The plug-in normally stop animations/sounds in this method.
+  def webPlugInStop
+    puts "[!] webPlugInStop called"
+  end
+
+  # Perform cleanup and prepare to be deallocated.
+  def webPlugInDestroy
+    puts "[!] webPlugInDestroy called"
+  end
+
+  # This is typically used to allow the plug-in to alter its appearance when selected
+  def webPlugInSetIsSelected(selected)
+    puts "[!] webPlugInSetIsSelected called: #{selected}"
+  end
+
+  # Returns the object that exposes the plug-in's interface.  The class of this object can implement
+  # methods from the WebScripting informal protocol.
+  def objectForWebScript
+    puts "[!] objectForWebScript called"
+    self
+  end
+
 end
